85f212e9b51870b95ad6da475a7991fbb44f5c40
ElyriaG <ElyriaG@users.noreply.github.com>
2019-05-17 14:16:37 -0500

    portfolio links up

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+$("#one").click(function() {
+    window.open('https://elyriag.github.io/Word-Guess-Game/', '_blank');
+});
+$("#two").click(function() {
+    window.open('https://elyriag.github.io/unit-4-game/', '_blank');
+});
+$("#three").click(function() {
+    window.open("https://elyriag.github.io/TriviaGame/", '_blank');
+});
+$("#four").click(function() {
+    window.open('https://elyriag.github.io/Giphy-API/', '_blank');
+});
+$("#five").click(function() {
+    window.open('https://elyriag.github.io/Train_Schedule/', '_blank');
+});
+$("#six").click(function() {
+    window.open('https://elyriag.github.io/Project-0/', '_blank');
+});
